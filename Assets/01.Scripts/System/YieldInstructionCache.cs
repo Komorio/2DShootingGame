636d3ea44a076091f611fcddef13ef022ece68e9
Komorio <twilivez@gmail.com>
2020-07-11 01:04:09 +0900

    [INITIAL] 프로젝트 초기화

---
+++ Assets/01.Scripts/System/YieldInstructionCache.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public static class YieldInstructionCache{
+    private static Dictionary<float, WaitForSeconds> waitForSecondDictionary = new Dictionary<float, WaitForSeconds>();
+    private static object waitUntil = null;
+
+    public static object WaitUntil => waitUntil;
+
+    public static WaitForSeconds WaitSeconds(float value){
+        if(!waitForSecondDictionary.ContainsKey(value)){
+            waitForSecondDictionary.Add(value, new WaitForSeconds(value));
+        }
+        
+        return waitForSecondDictionary[value];
+    }
+}
