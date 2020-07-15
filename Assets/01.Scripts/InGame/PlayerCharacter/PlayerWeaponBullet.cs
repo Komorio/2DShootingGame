45ca4957ad14d78244a824ddf0001a7a9b5f10a7
Komorio <twilivez@gmail.com>
2020-07-15 14:34:10 +0900

    [ADD] Weapon Bullet 추가

---
+++ Assets/01.Scripts/InGame/PlayerCharacter/PlayerWeaponBullet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWeaponBullet : MonoBehaviour
+{   
+    [Header("Values")]
+    [SerializeField]
+    private float _speed;
+    protected float speed => _speed;
+    public virtual void Execute(){
+        gameObject.SetActive(true);
+        ExecuteCoroutine().Start(this);
+    }
+
+    private IEnumerator ExecuteCoroutine(){
+        while(true){
+            gameObject.transform.Translate(Vector2.up * speed * Time.deltaTime);
+            yield return YieldInstructionCache.WaitUntil;
+        }
+    }
+
+    public virtual void Exit(){
+        gameObject.SetActive(false);
+    }
+}
