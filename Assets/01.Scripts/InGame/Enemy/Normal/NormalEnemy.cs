b9874093c2cacc8feafab86f135f8916119b0084
Komorio <twilivez@gmail.com>
2020-07-15 11:10:43 +0900

    [ADD] Add normal enemy script

---
+++ Assets/01.Scripts/InGame/Enemy/Normal/NormalEnemy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NormalEnemy : Enemy
+{   
+    private Vector2 moveVector;
+
+    protected override void Awake(){
+        moveVector = Vector2.down;
+    }
+
+    private void Update(){
+        if(!isActive){
+            this.enabled = false;
+        }
+
+        gameObject.transform.Translate(moveVector * speed * Time.deltaTime);       
+    }
+}
