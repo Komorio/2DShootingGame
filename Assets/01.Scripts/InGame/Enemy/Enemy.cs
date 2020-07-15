8934711e98f8ba8d8210aecd749108cf48bc23a7
Komorio <twilivez@gmail.com>
2020-07-15 11:09:52 +0900

    [ADD] Add enemy parent class & enemy scriptable

---
+++ Assets/01.Scripts/InGame/Enemy/Enemy.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [Header("Enemy Information")]
+    [SerializeField]
+    private EnemyInformation _enemyInformation;
+    protected EnemyInformation enemyInformation => _enemyInformation;
+
+    private int _defaultHp;
+    protected int defaultHp => _defaultHp;
+
+    private int _hp;
+    protected int hp => _hp;
+
+    private float _defaultSpeed;
+    protected float defaultSpeed => _defaultSpeed;
+
+    private float _speed;
+    protected float speed => _speed;
+
+    private int _score;
+    protected int score => _score;
+
+    private SpriteRenderer _spriteRenderer;
+    protected SpriteRenderer spriteRenderer => _spriteRenderer;
+
+    private bool _isActive;
+    protected bool isActive => _isActive;
+        
+    protected virtual void Awake(){
+        _enemyInformation.GetValues(out _defaultHp, out _defaultSpeed, out _score);
+        _spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        ObjectReset();
+    }
+
+    public virtual void ObjectActive(){
+        gameObject.SetActive(true);
+        _isActive = true;
+    }
+
+    protected virtual void Attack() { }
+
+    protected virtual void Move() { }
+
+    protected virtual void Death() { 
+        StageManager.instance.GameScore += score;
+    }
+
+    protected virtual void ObjectReset() { 
+        gameObject.SetActive(false);
+
+        _hp = _defaultHp;
+        _speed = _defaultSpeed;
+
+        _isActive = false;
+    }
+}
---
+++ Assets/01.Scripts/InGame/Enemy/EnemyInformation.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "EnemyInformation", menuName = "EnemyInformation", order = 0)]
+public class EnemyInformation : ScriptableObject {
+
+    [SerializeField]
+    private int _defaultHp;
+
+    [SerializeField]
+    private float _defaultSpeed;
+    
+    [SerializeField]
+    private int _score;
+
+    public void GetValues(out int defaultHp, out float defaultSpeed, out int score){
+        defaultHp = _defaultHp;
+        defaultSpeed = _defaultSpeed;
+        score = _score;
+    }
+}
