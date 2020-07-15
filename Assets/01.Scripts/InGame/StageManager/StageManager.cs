7ac04f5cc479395eca3b0e177c966dc88e06ed11
Komorio <twilivez@gmail.com>
2020-07-15 10:50:14 +0900

    [ADD] Score UI 및 StageManager, UIController 추가

---
+++ Assets/01.Scripts/InGame/StageManager/StageManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageManager : Singleton<StageManager>
+{
+    [HideInInspector]
+    public UIController uiController;
+
+    protected override void Awake(){
+        base.Awake();
+        
+        uiController = gameObject.GetComponent<UIController>();
+    }
+}
---
+++ Assets/01.Scripts/InGame/StageManager/UIController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class UIController : MonoBehaviour
+{
+    [Header("Objects")]
+    [SerializeField]
+    private Text scoreText;
+
+    public void SetScoreText(int scoreValue){
+        scoreText.text = scoreValue.ToString("D11");
+    }
+}
