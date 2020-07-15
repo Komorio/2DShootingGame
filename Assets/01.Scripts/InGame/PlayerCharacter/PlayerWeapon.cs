5eac9f739d828dac4470bbc629134c840795da3a
Komorio <twilivez@gmail.com>
2020-07-15 14:42:36 +0900

    [UPDATE] 공격 쿨다운 추가

--- Assets/01.Scripts/InGame/PlayerCharacter/PlayerWeapon.cs
+++ Assets/01.Scripts/InGame/PlayerCharacter/PlayerWeapon.cs
@@ -11,19 +11,48 @@ public class PlayerWeapon : MonoBehaviour
     private GameObject bulletObjects;
     private List<Bullet> bullets = new List<Bullet>();
 
+    private float defaultAttackCoolDown = 0.1f;
+    private float attackCoolDown;
+    private bool canAttack;
+    
     private void Awake() {
-        bullets = bulletObjects.GetComponentsInChildren<Bullet>(true).ToList();    
+        bullets = bulletObjects.GetComponentsInChildren<Bullet>(true).ToList();
+        attackCoolDown = defaultAttackCoolDown;    
+    }
+
+    private void Update(){
+        if(!canAttack){
+            if(attackCoolDown > 0){
+                attackCoolDown -= Time.unscaledDeltaTime;
+            } else {
+                attackCoolDown = defaultAttackCoolDown; 
+                canAttack = true;
+            }
+        }
     }
 
     public virtual void StrongAttack(){
-        Attack();
+       
     }
 
     public virtual void Attack(){
-        GetAvaliableBullet(bullets)?.Execute();
+        if(!canAttack){
+            return;
+        }
+        
+        Bullet generateBullet = GetAvaliableBullet(bullets);
+        
+        if(generateBullet == null){
+            return;
+        }
+
+        generateBullet.gameObject.transform.position = gameObject.transform.position;
+        generateBullet.Execute();
+
+        canAttack = false;
     }
 
-    protected Bullet GetAvaliableBullet(List<Bullet> bullet){
+    protected Bullet GetAvaliableBullet(List<Bullet> bullet){        
         for(int i = 0; i < bullet.Count; i++){
             if(!bullet[i].gameObject.activeInHierarchy){
                 return bullet[i];
