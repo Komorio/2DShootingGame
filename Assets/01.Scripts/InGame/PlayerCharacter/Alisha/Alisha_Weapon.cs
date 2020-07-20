c8c1817cacbd3460395e8ae18193fb227268d4e5
Komorio <twilivez@gmail.com>
2020-07-20 09:41:42 +0900

    [ADD] 캐릭터 엘리샤 추가
    00. 시간 멈추는 기능을 갖고 있음

---
+++ Assets/01.Scripts/InGame/PlayerCharacter/Alisha/Alisha_Weapon.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Alisha_Weapon : PlayerWeapon
+{
+    private bool isPause;
+    private float defaultPauseTime = 5.0f;
+    private float pauseTime;
+
+    private void LateUpdate() {
+        if(isPause){
+            if(pauseTime < defaultPauseTime){
+                pauseTime += Time.unscaledDeltaTime;
+                return;
+            }
+
+            pauseTime = 0.0f;
+            isPause = false;
+            Time.timeScale = 1.0f;
+        }
+    }
+
+    public override void StrongAttack(){
+        if(!isPause){
+            isPause = true;
+            Time.timeScale = 0.0f;
+        }
+    }
+}
